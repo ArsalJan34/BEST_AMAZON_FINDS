4e8b375516c253d1391ca40041a68ab66c13ae3c
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-29 14:51:37 +0500

    Changes made

---
+++ script.js
@@ -0,0 +1,14 @@
+ const searchInput = document.getElementById('searchInput');
+    const productCards = document.querySelectorAll('.card1');
+
+    searchInput.addEventListener('input', function() {
+      const searchValue = this.value.toLowerCase();
+      productCards.forEach(card => {
+        const name = card.querySelector('h3').textContent.toLowerCase();
+        if (name.includes(searchValue)) {
+          card.style.display = 'block';
+        } else {
+          card.style.display = 'none';
+        }
+      });
+    });
