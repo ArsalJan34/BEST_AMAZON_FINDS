50efaec777d2bc9babfe83e974b93259f8e6ac3d
ArsalJan34 <arsaljaan532@gmail.com>
2025-10-10 14:16:16 +0500

    Changes made

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function showSidebar(){
   const sidebar = document.querySelector('.sidebar')
   sidebar.style.display = 'flex'
 }
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
+}
 const searchInput = document.getElementById('searchInput');
 const productCards = document.querySelectorAll('.card1');
 
